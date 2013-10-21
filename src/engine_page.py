de90f623da8d40d0ded058d4d9466da118883e29
Douglas Schilling Landgraf <dougsland@redhat.com>
2013-10-21 13:02:40 -0400

    engine_page: replace log import
    Due http://gerrit.ovirt.org/#/c/18927/ that add a log module we must update
    engine_page.
    
    Change-Id: Ie8e07b94ae18e1e7fd0c89b9b005f8eb5c0fe7eb
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1019859
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/engine_page.py
+++ src/engine_page.py
@@ -21,8 +21,8 @@
 from ovirt.node import plugins, valid, ui, utils, app, exceptions
 from ovirt.node.config.defaults import NodeConfigFileSection
 from ovirt.node.plugins import Changeset
+from ovirt.node import log
 from . import config
-import logging
 import os
 import sys
 import traceback
@@ -33,7 +33,7 @@ Configure Engine
 """
 
 
-LOGGER = logging.getLogger(__name__)
+LOGGER = log.getLogger(__name__)
 
 
 class Plugin(plugins.NodePlugin):
@@ -169,7 +169,7 @@ class Plugin(plugins.NodePlugin):
             progress_dialog.run()
 
             # VDSM messes with logging, and we just reset it
-            app.configure_logging()
+            log.configure_logging()
 
         # Acts like a page reload
         return self.ui_content()
