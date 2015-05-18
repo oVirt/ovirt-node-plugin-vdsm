cedb79f6097691b2bde0b665456eacf059dfef02
Douglas Schilling Landgraf <dougsland@redhat.com>
2015-05-18 18:56:54 -0400

    autoreg: vdsm-tool register verb
    - Added support to vdsm-tool register verb
    - Added key management_server_fingerprint
    - Added key engine_admin_password
    
    See also:
    vdsm-tool: Add register verb
    https://gerrit.ovirt.org/#/c/40966/
    
    engine_page: vdsm-tool register verb integration
    https://gerrit.ovirt.org/#/c/41081/
    
    Change-Id: I7417dfcf4165d88e4e22b315a03d0e4ac8984571
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/41082
    Reviewed-by: Jenkins CI

--- autoinstall/autoreg.py
+++ autoinstall/autoreg.py
@@ -25,6 +25,7 @@ from ovirt.node.config.defaults import SSH
 from ovirt.node.setup.vdsm import engine_page
 from ovirt.node.utils import system
 
+# Log: /var/log/ovirt-node.log
 LOGGER = log.getLogger(__name__)
 ARGS = system.kernel_cmdline_arguments()
 
@@ -43,19 +44,27 @@ def is_karg_set(key, debug=True):
 def main():
     LOGGER.info("== autoinstall: starting validation for kernel arguments ==")
 
+    # For autoinstall using check-fqdn = False as previous autoinstall
+    # doesn't check CA cert
+    vdsm_tool_cmd = "vdsm-tool register --check-fqdn False "
+
     if "ovirt_vdsm_disable" in ARGS:
         LOGGER.info("autoinstall: ovirt_vdsm_disabled is set, nothing to do!")
         return 0
 
+    triggered_autoreg = False
     if is_karg_set("management_server"):
         # Updating OVIRT_MANAGEMENT_SERVER in /etc/default/ovirt
         engine_page.VDSM().update(server=ARGS["management_server"],
                                   port=None,
                                   cert_path=None)
+        vdsm_tool_cmd += "--engine-fqdn {s} ".format(
+                         s=ARGS["management_server"])
+        triggered_autoreg = True
 
     if is_karg_set("management_server_port"):
         if not is_karg_set("management_server", debug=False):
-            LOGGER.error("To use management_server_port karg is required"
+            LOGGER.error("To use management_server_port is required"
                          " to set management_server key too!")
             return -1
         else:
@@ -64,15 +73,35 @@ def main():
             engine_page.VDSM().update(server=ARGS["management_server"],
                                       port=ARGS["management_server_port"],
                                       cert_path=None)
+            vdsm_tool_cmd += "--engine-https-port {p} ".format(
+                             p=ARGS["management_server_port"])
+            triggered_autoreg = True
 
-    # For rhevm_admin_password use:
+    if is_karg_set("management_server_fingerprint"):
+        if not is_karg_set("management_server", debug=False):
+            LOGGER.error("To use management_server_fingerprint is required"
+                         " to set management_server key too!")
+            return -1
+        else:
+            vdsm_tool_cmd += "--fingerprint {f}".format(
+                             f=ARGS["management_server_fingerprint"])
+            triggered_autoreg = True
+
+    # For rhevm_admin_password/engine_admin_password use:
     # openssl passwd -1 to genereate the password
+    admin_pwd = None
     if is_karg_set("rhevm_admin_password"):
+        admin_pwd = "rhevm_admin_password"
+
+    if is_karg_set("engine_admin_password"):
+        admin_pwd = "engine_admin_password"
+
+    if admin_pwd:
         try:
             _functions.unmount_config("/etc/shadow")
             _functions.unmount_config("/etc/passwd")
             engine_page.execute_cmd("/usr/sbin/usermod -p %s root" %
-                                    ARGS["rhevm_admin_password"])
+                                    ARGS[admin_pwd])
 
             engine_page.execute_cmd("chage -E -1 root")
             utils.fs.Config().persist("/etc/shadow")
@@ -86,7 +115,17 @@ def main():
         SSH().update(pwauth=True)
         SSH().commit()
 
-    LOGGER.info("== autoinstall: finished ==")
+    if triggered_autoreg:
+        LOGGER.info("autoinstall: vdsm-tool register command")
+        LOGGER.info("{c}".format(c=vdsm_tool_cmd))
+        out, ret = engine_page.execute_cmd(vdsm_tool_cmd)
+        LOGGER.info("autoinstall: vdsm-tool ret: {o}".format(o=out))
+        if ret != 0:
+            LOGGER.error("autoinstall: vdsm-tool register command FAILED!")
+            LOGGER.error("Full log: /var/log/vdsm/register.log")
+            return ret
+
+    LOGGER.info("== autoinstall successfully finished ==")
 
     return 0
 
