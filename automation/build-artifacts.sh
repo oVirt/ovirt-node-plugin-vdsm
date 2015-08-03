7f813cffc51dd4643c07bb1802f24060b385314d
Tolik Litovsky <tlitovsk@redhat.com>
2015-08-03 17:43:39 +0300

    adjusting to the new CI framework
    Change-Id: I2038041c177ec353cc875a9468edb2e7485aa657
    Signed-off-by: Tolik Litovsky <tlitovsk@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/44327
    Continuous-Integration: Jenkins CI
    Reviewed-by: Fabian Deutsch <fabiand@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -2,17 +2,17 @@
 
 shopt -s nullglob
 
-rm -rf ../exported-artifacts/*
+rm -rf exported-artifacts/*
 rm -rf tmp.repos
 # generate automake/autoconf files
 ./autogen.sh
 
 # create rpm
 make rpms
-mkdir -p ../exported-artifacts/
+mkdir -p exported-artifacts/
 
 for file in $(find tmp.repos -iregex ".*\.\(tar\.gz\|rpm\)$"); do
     echo "Archiving $file"
-    mv "$file" ../exported-artifacts/
+    mv "$file" exported-artifacts/
 done
 
